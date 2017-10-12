a67c7ef0c5269b9616fdeda83c8903330831415c
u3shadow <u3shadow@gmail.com>
2017-10-12 17:23:56 +0800

    add modelattribute annotation

--- src/main/java/com/u3coding/mybatistest/resources/GreetingController.java
+++ src/main/java/com/u3coding/mybatistest/resources/GreetingController.java
@@ -2,6 +2,7 @@ package com.u3coding.mybatistest.resources;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +12,20 @@ import java.io.IOException;
 
 @Controller
 public class GreetingController {
+    @ModelAttribute
+    public void addAt(@RequestParam String name,Model model){
+        name = "model";
+        model.addAttribute("name",name);
+    }
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name",defaultValue = "world",required = false)String name, Model model) throws IOException {
         model.addAttribute("name",name);
         return "greeting";
     }
         @RequestMapping("/gree")
-    public String greet(@RequestParam(value="name",defaultValue = "world",required = false)String name, Model model) throws IOException {
-        model.addAttribute("name",name);
+    public String greet(@RequestParam(value="name",defaultValue = "world",required = false)String name,@ModelAttribute("name") String name1
+                        ,Model model) throws IOException {
+        model.addAttribute("name",name1);
         return "greeting";
     }
     @RequestMapping(value = "/uploadfile",headers = "content-type=multipart/*")
